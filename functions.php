56f2d721e5200a79c0ed08105d8e49f345b51df4
Fabiano <fabianocarvalho15@gmail.com>
2018-11-01 14:10:34 -0300

    Alterado para o tema pai tainacan-interface

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@ require_once ('functions/class-tainacanmuseuIndiothemeterm.php');
 
 // Estilos
 function museuindio_enqueue_styles() {
-    $parent_style = 'parent-style';
+    $parent_style = 'tainacan-interface';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style( 'tainacan-museudoindio',
     get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
