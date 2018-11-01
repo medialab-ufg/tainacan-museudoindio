73252ee7c51dc7eb50c17a919de19c7f1a5e6c54
Fabiano <fabianocarvalho15@gmail.com>
2018-11-01 15:23:30 -0300

    Added description in header banner and initialize the front page

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<!-- Get the banner to display -->
+<?php get_template_part( 'template-parts/bannerheader' ); ?>
+<!-- Get the menu if is create in panel admin -->
+<?php get_template_part( 'template-parts/menubellowbanner' ); ?>
+
+
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -10,6 +10,13 @@ function museuindio_enqueue_styles() {
     get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
+    wp_enqueue_style( 'style-museudoindio', get_stylesheet_directory_uri() . '/assets/scss/museudoindio.css' );
 }
 add_action( 'wp_enqueue_scripts', 'museuindio_enqueue_styles', 99 );
 
+function description_on_header_banner(){ ?>
+    <hr/>
+    <p>Aqui teremos uma breve descrição do repositório em si, do que se trata, histórico,
+resumo, etc.</p>
+<?php }
+add_action('tainacan-interface-banner-header-description', 'description_on_header_banner');
