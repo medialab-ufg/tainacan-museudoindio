ee4d493436d8d7e4bed6ae778cd19665132cc73a
Fabiano <fabianocarvalho15@gmail.com>
2018-11-09 14:27:35 -0200

    align menu and align on mobile the search and share icons

--- template-parts/menubanner-before.php
+++ template-parts/menubanner-before.php
@@ -1,6 +1,6 @@
 <?php if(has_nav_menu('MenuBannerBefore')) : ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-0">
-        <div class="container">
+        <div class="container-fluid max-large px-0 margin-one-column">
             <?php echo tainacan_get_logo(); ?> 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
