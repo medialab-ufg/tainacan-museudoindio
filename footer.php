4e0a35bfe0371b612552b93489cbbcd42f3ab270
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-12-14 16:44:03 -0200

    Change footer of logos and powered position

--- footer.php
+++ footer.php
@@ -1,15 +1,5 @@
 <?php if ( ! is_404() ) : ?>
 	<footer class="container-fluid p-4 p-sm-5 mt-5 tainacan-footer" style="padding-bottom: 0 !important;">
-		<?php if ( is_active_sidebar( 'tainacan-sidebar-footer' ) ) { ?>
-			<div class="row">
-				<div class="col-12 col-lg">
-					<ul class="p-4 d-lg-flex justify-content-md-center mb-md-0">
-						<?php dynamic_sidebar( 'tainacan-sidebar-footer' ); ?>
-					</ul>
-				</div>
-			</div>
-			<hr class="bg-scooter"/>
-		<?php } ?>
 		<div class="row p-4 tainacan-footer-info">
 			<div class="col text-white font-weight-normal">
 				<p class="tainacan-footer-info--blog">
@@ -39,10 +29,18 @@ if ( get_option( 'tainacan_blogphone' ) ) {
 } ?>
 				</p>
 			</div>
-			<div class="col-auto pr-0 pr-md-3 d-none d-md-block align-self-md-top">
-					<img src="<?php if ( get_theme_mod( 'tainacan_footer_logo' ) ) { echo esc_attr( get_theme_mod( 'tainacan_footer_logo' ) ); }else{ echo get_template_directory_uri() ?>/assets/images/logo-footer.svg<?php }?> ?>" class="tainacan-footer-info--logo" alt="">
-			</div>
-			<div class="col-12 tainacan-powered">
+		</div>
+        <hr class="bg-scooter"/>
+        <div class="row p-4 tainacan-mindio-footer--barra-logos text-white justify-content-around justify-content-sm-around align-items-center">
+			<div><img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-footer.svg" alt="" class="logo-tainacan"></div>
+			<div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/SAMI.svg" alt="" class="logo-sami"></div>
+			<div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/Museu_do_Indio.svg" alt="" class="logo-mindio"></div>
+            <div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/Funai.svg" alt="" class="logo-funai"></div>
+            <div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/Unesco.svg" alt="" class="logo-unesco"></div>
+			<div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/nome-governo-federal.png" alt="" class="logo-govfederal"></div>
+        </div>
+		<div class="row">
+			<div class="col-12 tainacan-powered mb-3 text-white">
 				<span>
 					<?php if ( true == get_theme_mod( 'tainacan_display_powered', false ) ) {
 						/* translators: 1: WordPress; 2: Tainacan*/
@@ -50,11 +48,6 @@ if ( get_option( 'tainacan_blogphone' ) ) {
 				</span>
 			</div>
 		</div>
-        <hr class="bg-scooter"/>
-        <div class="row p-4 tainacan-mindio-footer--barra-logos text-white justify-content-around justify-content-sm-start align-items-center">
-            <div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/Funai.svg" alt=""></div>
-            <div><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/Unesco.svg" alt=""></div>
-        </div>
 	</footer>
 <?php endif; ?>
 <?php wp_footer(); ?>
